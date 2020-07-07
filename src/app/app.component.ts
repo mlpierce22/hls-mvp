b2acfd92738e3fff0d44a4628e6a391d380c28fa
mlpierce22 <mlpierce22@gmail.com>
2020-07-07 10:17:31 -0700

    add tiling and have all streams run at once

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import Hls from 'hls.js'
 
 @Component({
@@ -6,44 +6,56 @@ import Hls from 'hls.js'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
  
-  width: number = 500;
-  height: number = 500;
+  width: number = 250;
+  height: number = 200;
+
+  liveStreams: Array<{}> = new Array<{}>();
+
+  liveStreamURLs = [
+    "http://sample.vodobox.com/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8",
+    "http://sample.vodobox.com/pipe_dream_tahiti/pipe_dream_tahiti.m3u8",
+    "http://sample.vodobox.com/we_are_blood_4k/we_are_blood_4k.m3u8",
+    "http://sample.vodobox.com/caminandes_1_4k/caminandes_1_4k.m3u8",
+    "http://sample.vodobox.com/big_buck_bunny_4k/big_buck_bunny_4k.m3u8",
+    "http://playertest.longtailvideo.com/adaptive/oceans_aes/oceans_aes.m3u8",
+    "http://playertest.longtailvideo.com/adaptive/captions/playlist.m3u8",
+  ]
   
   ngOnInit(): void {
-    const URL = "http://sample.vodobox.com/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8"
-    this.setupHls(URL)
-
-    /** Some other URL's you could use */
-    /*
-      http://sample.vodobox.com/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8
-      http://sample.vodobox.com/planete_interdite_hevc/planete_interdite_hevc.m3u8
-      http://sample.vodobox.com/pipe_dream_tahiti/pipe_dream_tahiti.m3u8
-      http://sample.vodobox.com/we_are_blood_4k/we_are_blood_4k.m3u8
-      http://sample.vodobox.com/caminandes_1_4k/caminandes_1_4k.m3u8
-      http://sample.vodobox.com/big_buck_bunny_4k/big_buck_bunny_4k.m3u8
-      http://playertest.longtailvideo.com/adaptive/oceans_aes/oceans_aes.m3u8
-      http://playertest.longtailvideo.com/adaptive/captions/playlist.m3u8
-    */
+    for (let i = 0; i < 16; i++) {
+      const randNum = Math.floor(Math.random() * this.liveStreamURLs.length - 1) + 1
+      console.log("rand num: ", randNum)
+      let url = this.liveStreamURLs[randNum]
+      console.log("url", url)
+      this.liveStreams.push({ id: i, url})
+    }
   }
 
-  setupHls(manifestURL) {
-    let video = document.getElementById("live-stream")
+  ngAfterViewInit(): void {
+    this.liveStreams.forEach((stream: any) => this.setupHls(stream.url, stream.id))
+  }
+
+  setupHls(manifestURL, id) {
+    let video = document.getElementById("live-stream-" + id) as HTMLVideoElement
+    video.muted = true
+    video.controls = false
+    video.loop = true
     if (Hls.isSupported()) {
       let hls = new Hls();
       hls.attachMedia(video)
       hls.on(Hls.Events.MEDIA_ATTACHED, () => {
+        console.log("url:", manifestURL, id)
         hls.loadSource(manifestURL);
         hls.on(Hls.Events.MANIFEST_PARSED, (event, data) => {
           console.log(data)
         });
       })
     }
-    else {
-      // TODO: implement canPlay for video (if the browser supports playing
-      // the m3u8 file directly)
-      console.log("fail")
+    else if (video.canPlayType('application/vnd.apple.mpegurl'))
+    {
+      video.src = manifestURL
     }
   }
 }
