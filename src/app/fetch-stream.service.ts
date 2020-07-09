bf40e7f6fbcb616380aff60de319de1d6b1a6a63
mlpierce22 <mlpierce22@gmail.com>
2020-07-09 13:13:47 -0700

    update service to fetch metadata too

--- src/app/fetch-stream.service.ts
+++ src/app/fetch-stream.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
+import { LiveStream } from './app.models';
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +9,38 @@ export class FetchStreamService {
 
   constructor() { }
 
-  /** Fetch the m3u8 based on an api key (TODO: FURTHER DEFINE THIS!) */
-  public fetch() {
+  /** Fetch the m3u8 and metaData based on an api key (TODO: FURTHER DEFINE THIS!) */
+  public fetch(): Observable<Partial<LiveStream>[]> {
     // TODO: fetch the actual m3u8 based on the api key
     return of([
-      "https://playertest.longtailvideo.com/adaptive/wowzaid3/playlist.m3u8",
-      "https://playertest.longtailvideo.com/adaptive/captions/playlist.m3u8",
-      "https://playertest.longtailvideo.com/adaptive/oceans_aes/oceans_aes.m3u8",
-      "https://b028.wpc.azureedge.net/80B028/Samples/0e8848ca-1db7-41a3-8867-fe911144c045/d34d8807-5597-47a1-8408-52ec5fc99027.ism/Manifest(format=m3u8-aapl-v3)",
-      "https://playertest.longtailvideo.com/adaptive/captions/playlist.m3u8",
-      "https://playertest.longtailvideo.com/adaptive/oceans_aes/oceans_aes.m3u8",
+      {
+        manifestUrl: "https://playertest.longtailvideo.com/adaptive/wowzaid3/playlist.m3u8",
+        online: true,
+        cameraName: "Long Tail Video - Captions",
+        timeStamp: Date.now(),
+        labels: ['HQ', 'San Mateo', 'Work'],
+      },
+      {
+        manifestUrl: "https://playertest.longtailvideo.com/adaptive/captions/playlist.m3u8",
+        online: true,
+        cameraName: "Long Tail Video - Captions",
+        timeStamp: Date.now(),
+        labels: ['HQ', 'San Mateo', 'Work'],
+      },
+      {
+        manifestUrl: "https://playertest.longtailvideo.com/adaptive/oceans_aes/oceans_aes.m3u8",
+        online: true,
+        cameraName: "Long Tail Video - Oceans",
+        timeStamp: Date.now(),
+        labels: ['HQ', 'San Mateo', 'Work'],
+      },
+      {
+        manifestUrl: "https://b028.wpc.azureedge.net/80B028/Samples/0e8848ca-1db7-41a3-8867-fe911144c045/d34d8807-5597-47a1-8408-52ec5fc99027.ism/Manifest(format=m3u8-aapl-v3)",
+        online: true,
+        cameraName: "Azure Edge",
+        timeStamp: Date.now(),
+        labels: ['HQ', 'San Mateo', 'Work'],
+      },
     ])
   }
 }
