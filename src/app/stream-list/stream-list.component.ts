de569f03ad381e630d5a96c167f3edae9b7f72fc
mlpierce22 <mlpierce22@gmail.com>
2020-07-08 18:09:54 -0700

    stream list holds streams

--- src/app/stream-list/stream-list.component.ts
+++ src/app/stream-list/stream-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { LiveStream, VideoDimensions } from '../app.models';
 
 @Component({
   selector: 'app-stream-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stream-list.component.scss']
 })
 export class StreamListComponent implements OnInit {
+
+  @Input() liveStreams: LiveStream[]
+
+  @Input() listedStreamDim: VideoDimensions
+
+  @Output() selectVideo: EventEmitter<number> = new EventEmitter<number>();
 
   constructor() { }
 
