5b11dce9cfeeb0c17dbf01d83dda0674568cbcf7
mlpierce22 <mlpierce22@gmail.com>
2020-07-10 14:21:49 -0700

    animate focused-stream down

--- src/app/focused-stream/focused-stream.component.ts
+++ src/app/focused-stream/focused-stream.component.ts
@@ -1,18 +1,39 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { LiveStream, VideoDimensions } from '../app.models';
 import { Subject } from 'rxjs';
+import { trigger, transition, animate, style } from '@angular/animations'
+
 
 @Component({
   selector: 'app-focused-stream',
   templateUrl: './focused-stream.component.html',
-  styleUrls: ['./focused-stream.component.scss']
+  styleUrls: ['./focused-stream.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({
+          maxHeight: '0px',
+        }),
+        animate('500ms ease-in', 
+        style({
+          maxHeight: '999px',
+        }))
+      ]),
+    ])
+  ],
 })
 export class FocusedStreamComponent implements OnInit {
 
   /** The big stream the user is focused on. */
-  @Input() focusedStream: LiveStream;
+  @Input() focusedStreamIndex: number;
 
+  @Input() liveStreams: LiveStream[];
+  
   @Input() focusedStreamDim: VideoDimensions;
+
+  @Output() toggleVideoSelect: EventEmitter<number> = new EventEmitter<number>();
+
+  @Output() search: EventEmitter<string> = new EventEmitter<string>();
 
 
   constructor() { }
