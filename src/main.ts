d561cbf21591040ec94367213740e7c1bee5e00b
mlpierce22 <mlpierce22@gmail.com>
2020-07-20 13:33:19 -0700

    Add local import to main.ts (this has to be fixed)

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+// TODO: Change path: could this be an http request??
+import "/Users/mpierce/Desktop/camio-stream-component/elements/camio-stream.js"
 
 if (environment.production) {
   enableProdMode();
