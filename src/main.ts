05c16449a958f8126032fa455c594171aa97108e
mlpierce22 <mlpierce22@gmail.com>
2020-07-24 09:51:15 -0700

    add import to include camio-stream

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 // TODO: Change path: could this be an http request??
-import "/Users/mpierce/Desktop/Camio/camio-stream-component/elements/camio-stream.js"
+import "../elements/camio-stream.js"
 
 if (environment.production) {
   enableProdMode();
